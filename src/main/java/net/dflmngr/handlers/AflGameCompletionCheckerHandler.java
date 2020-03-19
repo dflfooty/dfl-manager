4f9b4c9804e82b56672a48692c7bbc23ca709f97
Keith Lockwood <kwlockwo@gmail.com>
2020-03-20 01:01:10 +1100

    2020 scraping updates

--- src/main/java/net/dflmngr/handlers/AflGameCompletionCheckerHandler.java
+++ src/main/java/net/dflmngr/handlers/AflGameCompletionCheckerHandler.java
@@ -23,27 +23,27 @@ import net.dflmngr.utils.DflmngrUtils;
 
 public class AflGameCompletionCheckerHandler {
 	private LoggingUtils loggerUtils;
-	
+
 	private AflFixtureService aflFixtureService;
 	private GlobalsService globalsService;
-	
+
 	boolean isExecutable;
-	
+
 	String defaultMdcKey = "batch.name";
 	String defaultLoggerName = "batch-logger";
 	String defaultLogfile = "AflGameCompletionChecker";
-	
+
 	String mdcKey;
 	String loggerName;
 	String logfile;
-	
+
 	public AflGameCompletionCheckerHandler() {
 		aflFixtureService = new AflFixtureServiceImpl();
 		globalsService = new GlobalsServiceImpl();
-		
+
 		isExecutable = false;
 	}
-	
+
 	public void configureLogging(String mdcKey, String loggerName, String logfile) {
 		loggerUtils = new LoggingUtils(logfile);
 		this.mdcKey = mdcKey;
@@ -51,37 +51,41 @@ public class AflGameCompletionCheckerHandler {
 		this.logfile = logfile;
 		isExecutable = true;
 	}
-	
+
 	public void execute() {
 		try{
 			if(!isExecutable) {
 				configureLogging(defaultMdcKey, defaultLoggerName, defaultLogfile);
 				loggerUtils.log("info", "Default logging configured");
 			}
-			
+
 			loggerUtils.log("info", "AflGameCompletionChecker excuting ....");
-		
+
 			List<AflFixture> incompleteFixtures = aflFixtureService.getIncompleteFixtures();
-			
+
 			if(incompleteFixtures != null && !incompleteFixtures.isEmpty()) {
 				ZonedDateTime now = ZonedDateTime.now(ZoneId.of(DflmngrUtils.defaultTimezone));
-			
+
 				loggerUtils.log("info", "Incomplete AFL fixtures, fixtures={}", incompleteFixtures);
-				
+
 				List<AflFixture> completeFixtures = new ArrayList<>();
-				
+
 				String year = globalsService.getCurrentYear();
 				String statsUrl = globalsService.getAflStatsUrl();
-				
+
 				for(AflFixture fixture : incompleteFixtures) {
-					String homeTeam = fixture.getHomeTeam();
-					String awayTeam = fixture.getAwayTeam();
-					String aflRound = Integer.toString(fixture.getRound());
-					
-					String fullStatsUrl = statsUrl + "/" + year + "/" + aflRound + "/" + homeTeam.toLowerCase() + "-v-" + awayTeam.toLowerCase();
-					
+					//String homeTeam = fixture.getHomeTeam();
+					//String awayTeam = fixture.getAwayTeam();
+					//String aflRound = Integer.toString(fixture.getRound());
+
+					String roundStr = String.format("%02d", fixture.getRound());
+					String gameStr = String.format("%02d", fixture.getGame());
+
+					//String fullStatsUrl = statsUrl + "/" + year + "/" + aflRound + "/" + homeTeam.toLowerCase() + "-v-" + awayTeam.toLowerCase();
+					String fullStatsUrl = statsUrl + "/AFL" + year + roundStr + gameStr;
+
 					loggerUtils.log("info", "Checking for complete fixute at URL={}", fullStatsUrl);
-					
+
 					if(checkGame(fullStatsUrl)) {
 						fixture.setEndTime(now);
 						completeFixtures.add(fixture);
@@ -90,31 +94,31 @@ public class AflGameCompletionCheckerHandler {
 						loggerUtils.log("info", "Fixture not complete, fixture={}", fixture);
 					}
 				}
-				
+
 				if(!completeFixtures.isEmpty()) {
 					aflFixtureService.updateAll(completeFixtures, false);
 				}
 			} else {
 				loggerUtils.log("info", "All started AFL fixtures are complete");
 			}
-				
+
 			aflFixtureService.close();
 			globalsService.close();
-			
+
 			loggerUtils.log("info", "AflGameCompletionChecker comlpeted");
-			
+
 		} catch (Exception ex) {
 			loggerUtils.log("error", "Error in ... ", ex);
 		}
 	}
-	
+
 	private boolean checkGame(String statsUrl) throws Exception {
-		
+
 		boolean gameCompleted;
-		
+
 		int webdriverTimeout = globalsService.getWebdriverTimeout();
 		int webdriverWait = globalsService.getWebdriverWait();
-		
+
 		WebDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME) {
 	        @Override
 	        protected WebClient newWebClient(BrowserVersion version) {
@@ -125,29 +129,30 @@ public class AflGameCompletionCheckerHandler {
 	            return webClient;
 	        }
 		};
-		
+
 		driver.manage().timeouts().implicitlyWait(webdriverWait, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(webdriverTimeout, TimeUnit.SECONDS);
-		
+
 		try {
 			driver.get(statsUrl);
-			
-			if(driver.findElements(By.id("full-time-stats")).isEmpty()) {
-				gameCompleted = false;
-			} else {
+
+			//if(driver.findElements(By.id("full-time-stats")).isEmpty()) {
+				if(driver.findElement(By.className("styles_State-lxmyn6-2")).getText().equals("Full Time")) {
 				gameCompleted = true;
+			} else {
+				gameCompleted = false;
 			}
 		} catch (Exception ex) {
 			throw new Exception("Error Loading page, URL:" + statsUrl, ex);
 		} finally {
 			driver.quit();
 		}
-		
+
 		return gameCompleted;
 	}
-	
+
 	public static void main(String[] args) {
-		AflGameCompletionCheckerHandler testing = new AflGameCompletionCheckerHandler();							
+		AflGameCompletionCheckerHandler testing = new AflGameCompletionCheckerHandler();
 		testing.execute();
 	}
 }
