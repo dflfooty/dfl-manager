b3203f11a08cc25017e068356a31eb22ba8f075b
Keith Lockwood <kwlockwo@gmail.com>
2021-03-21 12:07:42 +1100

    fix start round

--- src/main/java/net/dflmngr/model/service/impl/DflTeamPredictedScoresServiceImpl.java
+++ src/main/java/net/dflmngr/model/service/impl/DflTeamPredictedScoresServiceImpl.java
@@ -10,67 +10,76 @@ import net.dflmngr.model.entity.DflTeamPredictedScores;
 import net.dflmngr.model.entity.keys.DflTeamPredictedScoresPK;
 import net.dflmngr.model.service.DflTeamPredictedScoresService;
 
-public class DflTeamPredictedScoresServiceImpl extends GenericServiceImpl<DflTeamPredictedScores, DflTeamPredictedScoresPK> implements DflTeamPredictedScoresService {
+public class DflTeamPredictedScoresServiceImpl extends
+		GenericServiceImpl<DflTeamPredictedScores, DflTeamPredictedScoresPK> implements DflTeamPredictedScoresService {
 	private DflTeamPredictedScoresDao dao;
-	
+
 	public DflTeamPredictedScoresServiceImpl() {
 		dao = new DflTeamPredictedScoresDaoImpl();
 		super.setDao(dao);
 	}
-	
+
 	public DflTeamPredictedScores getTeamPredictedScoreForRound(String teamCode, int round) {
 		DflTeamPredictedScoresPK pk = new DflTeamPredictedScoresPK();
 		pk.setRound(round);
 		pk.setTeamCode(teamCode);
 		DflTeamPredictedScores teamPredictedScore = get(pk);
+
+		if (teamPredictedScore == null || teamPredictedScore.getPredictedScore() == 0) {
+			teamPredictedScore = new DflTeamPredictedScores();
+			teamPredictedScore.setRound(round);
+			teamPredictedScore.setTeamCode(teamCode);
+			teamPredictedScore.setPredictedScore(550);
+		}
+
 		return teamPredictedScore;
 	}
-	
+
 	public List<DflTeamPredictedScores> getForRound(int round) {
 		List<DflTeamPredictedScores> teamPredictedScores = dao.findForRound(round);
 		return teamPredictedScores;
 	}
-	
+
 	public Map<String, DflTeamPredictedScores> getForRoundWithKey(int round) {
 		Map<String, DflTeamPredictedScores> teamPredictedScoresWithKey = new HashMap<>();
 		List<DflTeamPredictedScores> predictedScores = getForRound(round);
-		
-		for(DflTeamPredictedScores teamScore : predictedScores) {
+
+		for (DflTeamPredictedScores teamScore : predictedScores) {
 			teamPredictedScoresWithKey.put(teamScore.getTeamCode(), teamScore);
 		}
-		
+
 		return teamPredictedScoresWithKey;
 	}
-	
+
 	public List<DflTeamPredictedScores> getAllForRound(int round) {
 		List<DflTeamPredictedScores> predictedScores = dao.findAllForRound(round);
 		return predictedScores;
 	}
-	
-	public void replaceTeamForRound(int round, String teamCode,  DflTeamPredictedScores predictedScore) {
+
+	public void replaceTeamForRound(int round, String teamCode, DflTeamPredictedScores predictedScore) {
 		dao.beginTransaction();
 		DflTeamPredictedScores preidction = getTeamPredictedScoreForRound(teamCode, round);
-		if(preidction != null) {
+		if (preidction != null) {
 			delete(preidction);
 		}
 		dao.flush();
 		dao.commit();
-		
+
 		insert(predictedScore);
 	}
-	
+
 	public void replaceAllForRound(int round, List<DflTeamPredictedScores> predictedScores) {
 		dao.beginTransaction();
-		
+
 		List<DflTeamPredictedScores> existingPredictions = getAllForRound(round);
-		for(DflTeamPredictedScores preidction : existingPredictions) {
+		for (DflTeamPredictedScores preidction : existingPredictions) {
 			delete(preidction);
 		}
-		
+
 		dao.flush();
-		
+
 		insertAll(predictedScores, true);
-		
+
 		dao.commit();
 	}
 }
