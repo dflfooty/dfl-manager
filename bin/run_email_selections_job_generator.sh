c83963e524fac9cd924225680678541b1566e3c9
Keith Lockwood <kwlockwo@gmail.com>
2023-03-14 16:39:28 +1100

    2023 updates

--- bin/run_email_selections_job_generator.sh
+++ bin/run_email_selections_job_generator.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export CLASSPATH=$APP_HOME/target/dflmngr.jar:$APP_HOME/target/dependency/*
+echo $CLASSPATH
 java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EmailSelectionsJobGenerator
