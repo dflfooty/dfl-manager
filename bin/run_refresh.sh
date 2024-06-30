38a4094dc144af591439c2deced3c321ac00a0e0
Keith Lockwood <kwlockwo@gmail.com>
2024-06-30 10:23:49 +1000

    update deps

--- bin/run_refresh.sh
+++ bin/run_refresh.sh
@@ -6,18 +6,18 @@ run=$1
 
 if [[ $run == "now" ]]; then
     echo "Running Now"
-    java -classpath $CLASSPATH net.dflmngr.handlers.AflFixtureLoaderHandler
-    java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler
-    java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator
-    java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator
+    java -classpath $CLASSPATH net.dflmngr.handlers.AflFixtureLoaderHandler && \
+    java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler && \
+    java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator &&\
+    java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator && \
     java -classpath $CLASSPATH net.dflmngr.scheduler.generators.ResultsJobGenerator
 else
     if [[ $(date +%u) -eq 1 ]]; then
         echo "Monday Running"
-        java -classpath $CLASSPATH net.dflmngr.handlers.AflFixtureLoaderHandler
-        java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler
-        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator
-        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator
+        java -classpath $CLASSPATH net.dflmngr.handlers.AflFixtureLoaderHandler && \
+        java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler && \
+        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator && \
+        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator && \
         java -classpath $CLASSPATH net.dflmngr.scheduler.generators.ResultsJobGenerator
     else
         echo "Not Monday Skipping"
